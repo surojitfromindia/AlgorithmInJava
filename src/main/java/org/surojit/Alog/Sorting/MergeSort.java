629c9c11f2489155d20cb0f45c7f5fd779b75fc3
Surojit Paul <surojit99923@gmail.com>
2023-08-27 20:53:15 +0530

    Merge sort added.

---
+++ src/main/java/org/surojit/Alog/Sorting/MergeSort.java
@@ -0,0 +1,64 @@
+package org.surojit.Alog.Sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    private final int[] array;
+
+    public MergeSort(int[] array) {
+        this.array = array;
+    }
+
+
+    void merge(int[] mergedArray, int start, int middle, int end) {
+        int[] leftSortedArray = Arrays.copyOfRange(mergedArray, start, middle + 1);
+        int[] rightSortedArray = Arrays.copyOfRange(mergedArray, middle + 1, end + 1);
+        int lAL = leftSortedArray.length;
+        int rAL = rightSortedArray.length;
+        int leftPointer = 0;
+        int rightPointer = 0;
+        int mergePointer = start;
+        while (leftPointer < lAL && rightPointer < rAL) {
+            if (leftSortedArray[leftPointer] < rightSortedArray[rightPointer]) {
+                mergedArray[mergePointer] = leftSortedArray[leftPointer];
+                leftPointer++;
+            } else {
+                mergedArray[mergePointer] = rightSortedArray[rightPointer];
+                rightPointer++;
+            }
+            mergePointer++;
+        }
+        // copy the rest of the leftSortedArray
+        while (leftPointer < lAL) {
+            mergedArray[mergePointer] = leftSortedArray[leftPointer];
+            leftPointer++;
+            mergePointer++;
+        }
+
+        while (rightPointer < rAL) {
+            mergedArray[mergePointer] = rightSortedArray[rightPointer];
+            rightPointer++;
+            mergePointer++;
+        }
+    }
+
+
+    public int[] sort() {
+        mergeSort(array, 0, array.length - 1);
+        return array;
+    }
+
+    void mergeSort(int[] a, int start, int end) {
+        if (start < end) {
+            int middle = (start + end) / 2;
+            mergeSort(a, start, middle);
+            mergeSort(a, middle + 1, end);
+            merge(a, start, middle, end);
+
+        }
+
+    }
+
+
+}
--- src/main/java/org/surojit/Main.java
+++ src/main/java/org/surojit/Main.java
@@ -2,25 +2,31 @@ package org.surojit;
 
 import org.surojit.Alog.Sorting.BubbleSort;
 import org.surojit.Alog.Sorting.InsertionSort;
+import org.surojit.Alog.Sorting.MergeSort;
 import org.surojit.Alog.Sorting.SelectionSort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] unsortedArray = {4,76,9,3,22,90,4,65};
+        int[] unsortedArray = {4, 76, 9, 3, 22, 90, 4, 65};
 
         System.out.println("Bubble sort");
         BubbleSort bubbleSort = new BubbleSort(unsortedArray);
-        System.out.println("normal>"+Arrays.toString(bubbleSort.sort()));
-        System.out.println("optimised>"+Arrays.toString(bubbleSort.sortOptimised()));
+        System.out.println("normal>" + Arrays.toString(bubbleSort.sort()));
+        System.out.println("optimised>" + Arrays.toString(bubbleSort.sortOptimised()));
 
         System.out.println("Selection sort");
         SelectionSort selectionSort = new SelectionSort(unsortedArray);
-        System.out.println("normal>"+ Arrays.toString(selectionSort.sort()));
+        System.out.println("normal>" + Arrays.toString(selectionSort.sort()));
 
         System.out.println("Insert sort");
         InsertionSort insertionSort = new InsertionSort(unsortedArray);
-        System.out.println("normal>"+ Arrays.toString(insertionSort.sort()));
+        System.out.println("normal>" + Arrays.toString(insertionSort.sort()));
+
+        System.out.println("Merge sort");
+        MergeSort mergeSort = new MergeSort(unsortedArray);
+        System.out.println(Arrays.toString(mergeSort.sort()));
+
     }
 }
